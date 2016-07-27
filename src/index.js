eb76c98a56c9271147036950a8544e6cb163b0f2
Vitaly Aminev <AVVS@users.noreply.github.com>
2016-07-27 16:59:47 +0300

    feat: withExtraArgument based on original repo (#2)
    * Add withExtraArgument()
    
    Thanks to @Danmer, closes #1

--- src/index.js
+++ src/index.js
@@ -4,19 +4,24 @@ function isFunction(fn) {
   return typeof fn === 'function';
 }
 
-export default function thunkMiddleware({ dispatch, getState }) {
-  return next => action => {
+function createThunkMiddleware(extraArgument) {
+  return ({ dispatch, getState }) => next => action => {
     if (isFunction(action)) {
-      return action(dispatch, getState);
+      return action(dispatch, getState, extraArgument);
     }
 
     if (isFSA(action) && isFunction(action.payload)) {
       return next({
         ...action,
-        payload: action.payload(dispatch, getState),
+        payload: action.payload(dispatch, getState, extraArgument),
       });
     }
 
     return next(action);
   };
 }
+
+const thunk = createThunkMiddleware();
+thunk.withExtraArgument = createThunkMiddleware;
+
+export default thunk;
